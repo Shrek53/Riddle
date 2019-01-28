6c52f9b30d8e87c2519447a8bc123203d0c2eaf4
shrek53 <apon.ruet@gmail.com>
2019-01-28 09:32:16 +0600

    cleanup some extra lines

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -19,6 +19,7 @@ const SettingsStack = createStackNavigator({
     Settings: SettingsScreen,
   });
 
+
 // const MainBottomTab = createMaterialTopTabNavigator({
 //     Home : HomeStack,
 //     Settings: SettingsStack,
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -10,25 +10,16 @@ import {
   Button
 } from 'react-native';
 import {Header,Left,Right,Body,Icon,Title} from 'native-base';
-// import { MonoText } from '../components/StyledText';
 
 
 export default class SignInScreen extends React.Component {
-//   static navigationOptions = {
-//     header: null,
-//     drawerIcon:({tintColor}) => (
-//       <Icon name='home' style={{justifyContent: "flex-start",fontSize:24,color:tintColor}}/>
-//     )
-//   };
-
   render() {
     return (
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
             <View style={styles.welcomeContainer}>
               <Text style={styles.getStartedText}>
                 SignIn page.
-              </Text>
-              
+              </Text>             
             </View>
         </ScrollView>
     );
@@ -40,13 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 30,
   },
@@ -55,71 +39,10 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
   getStartedText: {
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  }
 });
