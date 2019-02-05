4118f03040685e555299b973112c231478a52a70
shrek53 <apon.ruet@gmail.com>
2019-02-06 02:00:53 +0600

    option buttons replaced with touchableOpacity

--- src/components/RiddleDetailsComponent.js
+++ src/components/RiddleDetailsComponent.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
   Dimensions,
   Button
 } from "react-native";
@@ -54,18 +55,22 @@ export default class RiddleDetailsComponent extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <View style={styles.middle}>
+      <View style={styles.container}>
+        <View style={styles.stat_bar}>
+          <Text>{this.state.riddle_group_pos+1}/{this.state.riddle_group_length}</Text>
+        </View>
         <View style={styles.question_block}>
           <Text style={styles.question}>{this.state.riddle.question}</Text>
         </View>
         <View style={styles.answers_block}>
           {this.state.riddle.options.map(item => (
             <View style={styles.answer_button_wrapper} key={item}>
-              <Button
-                title={item}
+              <TouchableOpacity
                 style={styles.answer_button}
                 onPress={this.level_btn_clicked}
-              />
+              >
+              <Text>{item}</Text>
+              </TouchableOpacity>
             </View>
           ))}
         </View>
@@ -75,27 +80,33 @@ export default class RiddleDetailsComponent extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  middle: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  container: {
+    flex: 1
+  },
+  stat_bar:{
+    flex:1,
+    justifyContent:'flex-start'
   },
   question_block: {
-    flex: 1,
+    flex: 3,
+    backgroundColor:"#000000",
     justifyContent: "center",
     alignItems: "center",
-    padding: 5
+    // padding: 5,
   },
   question: {
-    fontSize: 24
+    fontSize: 24,
+    color:'white'
   },
   answers_block: {
-    flex: 1,
+    flex: 6,
     justifyContent: "flex-start",
     alignItems: "center"
   },
   answer_button_wrapper: {
     padding: 10
   },
-  answer_button: {}
+  answer_button: {
+    // backgroundColor:'#000000'
+  }
 });
